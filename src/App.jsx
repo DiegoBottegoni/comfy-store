d57b65a34649c521f08453aa19ff9af447069b1d
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-02-15 21:22:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
+    </>
+  );
+};
+export default App;
