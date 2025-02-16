1e6811542a590aff2d39ab2dfbe49038e2d84125
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-02-15 21:29:06 -0300

    update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 import daisyui from "daisyui";
 export default {
   //...
-  plugins: [daisyui],
+  plugins: [daisyui, require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
